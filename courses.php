70fff4e88c52c2ac2ffed312699866cb3d9b8fc6
Bayu Muhammad Iqbal <bayuiqballl13@gmail.com>
2020-01-30 01:13:43 +0700

    first commit

---
+++ courses.php
@@ -0,0 +1,89 @@
+<?php
+require_once 'view/header.php';
+?>
+<!-- ================ start banner Area ================= -->
+<section class="banner-area">
+  <div class="container">
+    <div class="row justify-content-center align-items-center">
+      <div class="col-lg-12 banner-right">
+        <h1 class="text-white">
+          Courses
+        </h1>
+        <p class="mx-auto text-white  mt-20 mb-40">
+          In the history of modern astronomy, there is probably no one greater leap forward than the building.
+        </p>
+        <div class="link-nav">
+          <span class="box">
+            <a href="index.php">Home </a>
+            <i class="lnr lnr-arrow-right"></i>
+            <a href="courses.php">Courses</a>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================ End banner Area ================= -->
+
+<!-- ================ Start Feature Area ================= -->
+<section class="feature-area">
+  <div class="container-fluid">
+    <div class="feature-inner row">
+      <div class="col-lg-2 col-md-6">
+        <div class="feature-item d-flex">
+          <i class="ti-book"></i>
+          <div class="ml-20">
+            <h4>New Classes</h4>
+            <p>
+              In the history of modern astronomy, there is probably no one greater leap forward.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-2 col-md-6">
+        <div class="feature-item d-flex">
+          <i class="ti-cup"></i>
+          <div class="ml-20">
+            <h4>Top Courses</h4>
+            <p>
+              In the history of modern astronomy, there is probably no one greater leap forward.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-2 col-md-6">
+        <div class="feature-item d-flex border-right-0">
+          <i class="ti-desktop"></i>
+          <div class="ml-20">
+            <h4>Full E-Books</h4>
+            <p>
+              In the history of modern astronomy, there is probably no one greater leap forward.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ================ End Feature Area ================= -->
+
+<!-- ================ Start Popular Course Area ================= -->
+<section class="popular-course-area section-gap">
+  <div class="container-fluid">
+    <div class="row justify-content-center section-title">
+      <div class="col-lg-12">
+        <h2>
+          Popular Courses <br />
+          Available Right Now
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
+<?php
+require_once 'view/footer.php';
+?>
---
+++ view/header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="view/img/fav.png" />
+    <!-- Author Meta -->
+    <meta name="author" content="colorlib" />
+    <!-- Meta Description -->
+    <meta name="description" content="" />
+    <!-- Meta Keyword -->
+    <meta name="keywords" content="" />
+    <!-- meta character set -->
+    <meta charset="UTF-8" />
+    <!-- Site Title -->
+    <title>Eclipse Education</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:900|Roboto:400,400i,500,700" rel="stylesheet" />
+    <!--
+      CSS
+      =============================================
+    -->
+    <link rel="stylesheet" href="view/css/linearicons.css" />
+    <link rel="stylesheet" href="view/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="view/css/bootstrap.css" />
+    <link rel="stylesheet" href="view/css/magnific-popup.css" />
+    <link rel="stylesheet" href="view/css/owl.carousel.css" />
+    <link rel="stylesheet" href="view/css/nice-select.css">
+    <link rel="stylesheet" href="view/css/hexagons.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/themify-icons/0.1.2/css/themify-icons.css" />
+    <link rel="stylesheet" href="view/css/main.css" />
+</head>
+
+<body>
+    <!-- ================ Start Header Area ================= -->
+    <header class="default-header">
+        <nav class="navbar navbar-expand-lg  navbar-light">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">
+                    <img src="view/img/logo.png" alt="" />
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="lnr lnr-menu"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-end align-items-center" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="courses.php">Courses</a></li>
+                        <!-- Dropdown -->
+                        <li><a href="#test-form" class="login popup-with-form">Login</a></li>
+
+
+                        <li>
+                            <button class="search">
+                                <span class="lnr lnr-magnifier" id="search"></span>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="search-input" id="search-input-box">
+            <div class="container">
+                <form class="d-flex justify-content-between">
+                    <input type="text" class="form-control" id="search-input" placeholder="Search Here" />
+                    <button type="submit" class="btn"></button>
+                    <span class="lnr lnr-cross" id="close-search" title="Close Search"></span>
+                </form>
+            </div>
+        </div>
+    </header>
+    <!-- ================ End Header Area ================= -->
